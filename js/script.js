43aca90bb672010911aed8d7cfbba6d8c170a7bb
SheeranJL <lewisjsheeran@protonmail.com>
2021-09-22 18:31:43 +1000

    general updates

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let employees = [];
+let urlAPI = `https://randomuser.me/api/?results=12&inc=name, picture,
+email, location, phone, dob &noinfo &nat=US`
+let gridContainer = document.querySelector('.grid-container');
+let overlay = document.querySelector('.overlay');
+let modelContainer = document.querySelector('.modal-content');
+let modelClose = document.querySelector('.modal-close');
